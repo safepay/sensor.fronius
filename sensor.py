772e909d5cb1e66f44b5cb72cad091c9df880afc
Richard Mann <31983230+safepay@users.noreply.github.com>
2019-07-05 17:10:20 +1000

    Initial Commit

---
+++ sensor.py
@@ -0,0 +1,167 @@
+"""Support for the Fronius Inverter."""
+import logging
+from datetime import timedelta
+
+import requests
+import voluptuous as vol
+
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import (
+    CONF_IP_ADDRESS, CONF_MONITORED_CONDITIONS, CONF_NAME, CONF_DEVICE, CONF_SCOPE, CONF_DATA_COLLECTION
+    )
+from homeassistant.helpers.entity import Entity
+from homeassistant.util import Throttle
+
+_RESOURCE = 'http://{}/solar_api/v1/GetInverterRealtimeData.cgi'
+_LOGGER = logging.getLogger(__name__)
+
+ATTRIBUTION = "Fronius Inverter Data"
+
+CONF_IP_ADDRESS = 'ip_address'
+CONF_SCOPE = 'scope'
+CONF_DEVICE = 'device'
+
+SCOPE_TYPES = ['device', 'system']
+
+DEFAULT_SCOPE = 'device'
+DEFAULT_DEVICE = '0'
+DEFAULT_DATA_COLLECTION = 'CommonInverterData'
+
+MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=5)
+
+# Key: ['json_key', 'name', unit, icon]
+SENSOR_TYPES = {
+    'ac_power': ['PAC', 'AC Power', 'W', 'mdi:solar-power'],
+    'ac_current': ['IAC', 'AC Current', 'A', 'mdi:solar-power'],
+    'ac_voltage': ['UAC', 'AC Voltage', 'V', 'mdi:solar-power'],
+    'ac_frequency': ['FAC', 'AC Frequency', 'Hz', 'mdi:solar-power'],
+    'dc_current': ['IDC', 'DC Current', 'A', 'mdi:solar-power'],
+    'dc_voltage': ['UDC', 'DC Voltage', 'V', 'mdi:solar-power'],
+    'day_energy': ['DAY_ENERGY', 'Day Energy', 'Wh', 'mdi:solar-power'],
+    'year_energy': ['YEAR_ENERGY', 'Year Energy', 'Wh', 'mdi:solar-power'],
+    'total_energy': ['TOTAL_ENERGY', 'Total Energy', 'Wh', 'mdi:solar-power'],
+    'device_status': ['DeviceStatus', 'Device Status', None, 'mdi:solar-power']
+}
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_IP_ADDRESS): cv.string,
+    vol.Optional(CONF_SCOPE, default=DEFAULT_SCOPE):
+        vol.In(SCOPE_TYPES),
+    vol.Optional(CONF_DEVICE, default=DEFAULT_DEVICE): cv.string,
+    vol.Optional(CONF_NAME, default='Fronius'): cv.string,
+    vol.Optional(CONF_DATA_COLLECTION, default=DEFAULT_DATA_COLLECTION): cv.string,
+    vol.Required(CONF_MONITORED_CONDITIONS, default=list(SENSOR_TYPES)):
+        vol.All(cv.ensure_list, [vol.In(SENSOR_TYPES)]),
+})
+
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up the Fronius inverter sensor."""
+
+    ip_address = config[CONF_IP_ADDRESS]
+    scope = config.get(CONF_SCOPE)
+    device = config.get(CONF_DEVICE)
+    data_collection = config.get(CONF_DATA_COLLECTION)
+    name = config.get(CONF_NAME)
+
+    fronius_data = FroniusData(ip_address, scope, device, data_collection)
+
+    try:
+        fronius_data.update()
+    except ValueError as err:
+        _LOGGER.error("Received error from inverter: %s", err)
+        return
+
+    dev = []
+    for variable in config[CONF_MONITORED_CONDITIONS]:
+        dev.append(FroniusSensor(fronius_data, name, variable))
+
+    add_entities(dev, True)
+
+
+class FroniusSensor(Entity):
+    """Implementation of the Fronius inverter sensor."""
+
+    def __init__(self, inverter_data, name, sensor_type):
+        """Initialize the sensor."""
+        self._client = name
+        self._json_key = SENSOR_TYPES[sensor_type][0]
+        self._name = SENSOR_TYPES[sensor_type][1]
+        self._type = sensor_type
+        self._state = None
+        self._unit = SENSOR_TYPES[sensor_type][2]
+        self._data = inverter_data
+        self._icon = SENSOR_TYPES[sensor_type][3]
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return '{} {}'.format(self._client, self._name)
+
+    @property
+    def state(self):
+        """Return the state of the device."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement of this entity, if any."""
+        return self._unit
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes."""
+        attrs = {ATTR_ATTRIBUTION: ATTRIBUTION}
+        return attrs
+
+    @property
+    def icon(self):
+        """Icon to use in the frontend, if any."""
+        return self._icon
+
+    def update(self):
+        """Get the latest data from inverter and update the states."""
+        self._data.update()
+        if not self._data:
+            _LOGGER.info("Didn't receive data from the inverter")
+            return
+
+        # Read data
+        self._state = self._data.latest_data[self._json_key].get("Value")
+
+
+class FroniusData:
+    """Handle Fronius API object and limit updates."""
+
+    def __init__(self, ip_address, scope, device, data_collection):
+        """Initialize the data object."""
+        self._ip_address = ip_address
+        self._scope = scope
+        self._device = device
+        self._data_collection = data_collection
+
+    def _build_url(self):
+        """Build the URL for the requests."""
+        url = _RESOURCE.format(self._ip_address)
+        _LOGGER.debug("Fronius URL: %s", url)
+        return url
+
+    @property
+    def latest_data(self):
+        """Return the latest data object."""
+        if self._data:
+            return self._data
+        return None
+
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    def update(self):
+        """Get the latest data from inverter."""
+        URLParams = [
+            ("Scope", self._scope),
+            ("DeviceId", self._device),
+            ("DataCollection", self._data_collection)
+        ]
+        result = requests.get(self._build_url(), params=URLParams, timeout=10).json()
+        self._data = result['Data']
+        return
