7f2ff3c661df80753333ea38b36f2e62284dc85b
Richard Mann <31983230+safepay@users.noreply.github.com>
2019-07-08 07:33:53 +1000

    Changed Device to Device_ID throughout

--- sensor.py
+++ sensor.py
@@ -23,14 +23,14 @@ ATTRIBUTION = "Fronius Inverter Data"
 CONF_NAME = 'name'
 CONF_IP_ADDRESS = 'ip_address'
 CONF_SCOPE = 'scope'
-CONF_DEVICE = 'device'
+CONF_DEVICE_ID = 'device_id'
 CONF_DATA_COLLECTION = 'data_collection'
 
 SCOPE_TYPES = ['device', 'system']
 DATA_COLLECTION_TYPES = ['CumulationInverterData', 'CommonInverterData', '3PInverterData', 'MinMaxInverterData']
 
 DEFAULT_SCOPE = 'device'
-DEFAULT_DEVICE = '1'
+DEFAULT_DEVICE_ID = '1'
 DEFAULT_DATA_COLLECTION = 'CommonInverterData'
 
 MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=5)
@@ -52,7 +52,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_IP_ADDRESS): cv.string,
     vol.Optional(CONF_SCOPE, default=DEFAULT_SCOPE):
         vol.In(SCOPE_TYPES),
-    vol.Optional(CONF_DEVICE, default=DEFAULT_DEVICE): cv.string,
+    vol.Optional(CONF_DEVICE_ID, default=DEFAULT_DEVICE_ID): cv.string,
     vol.Optional(CONF_NAME, default='Fronius'): cv.string,
     vol.Optional(CONF_DATA_COLLECTION, default=DEFAULT_DATA_COLLECTION):
         vol.In(DATA_COLLECTION_TYPES),
@@ -66,11 +66,11 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 
     ip_address = config[CONF_IP_ADDRESS]
     scope = config.get(CONF_SCOPE)
-    device = config.get(CONF_DEVICE)
+    device_id = config.get(CONF_DEVICE_ID)
     data_collection = config.get(CONF_DATA_COLLECTION)
     name = config.get(CONF_NAME)
 
-    fronius_data = FroniusData(ip_address, scope, device, data_collection)
+    fronius_data = FroniusData(ip_address, scope, device_id, data_collection)
 
     try:
         fronius_data.update()
@@ -80,7 +80,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 
     dev = []
     for variable in config[CONF_MONITORED_CONDITIONS]:
-        dev.append(FroniusSensor(fronius_data, name, variable, scope, device))
+        dev.append(FroniusSensor(fronius_data, name, variable, scope, device_id))
 
     add_entities(dev, True)
 
@@ -88,7 +88,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 class FroniusSensor(Entity):
     """Implementation of the Fronius inverter sensor."""
 
-    def __init__(self, inverter_data, name, sensor_type, scope, device):
+    def __init__(self, inverter_data, name, sensor_type, scope, device_id):
         """Initialize the sensor."""
         self._client = name
         self._json_key = SENSOR_TYPES[sensor_type][0]
@@ -96,7 +96,7 @@ class FroniusSensor(Entity):
         self._type = sensor_type
         self._state = None
         self._scope = scope
-        self._device = device
+        self._device_id = device_id
         self._unit = SENSOR_TYPES[sensor_type][2]
         self._data = inverter_data
         self._icon = SENSOR_TYPES[sensor_type][3]
@@ -144,11 +144,11 @@ class FroniusSensor(Entity):
 class FroniusData:
     """Handle Fronius API object and limit updates."""
 
-    def __init__(self, ip_address, scope, device, data_collection):
+    def __init__(self, ip_address, scope, device_id, data_collection):
         """Initialize the data object."""
         self._ip_address = ip_address
         self._scope = scope
-        self._device = device
+        self._device_id = device_id
         self._data_collection = data_collection
 
     def _build_url(self):
@@ -169,7 +169,7 @@ class FroniusData:
         """Get the latest data from inverter."""
         URLParams = [
             ("Scope", self._scope),
-            ("DeviceId", self._device),
+            ("DeviceId", self._device_id),
             ("DataCollection", self._data_collection)
         ]
         
